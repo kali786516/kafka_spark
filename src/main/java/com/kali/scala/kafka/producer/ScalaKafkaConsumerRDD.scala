8aa78c5183a43080a54936d4b54029864be0c947
kali786516 <kali.tummala@gmail.com>
2016-02-02 20:26:44 +0000

    offset based consumer....

--- src/main/java/com/kali/scala/kafka/producer/ScalaKafkaConsumerRDD.scala
+++ src/main/java/com/kali/scala/kafka/producer/ScalaKafkaConsumerRDD.scala
@@ -5,11 +5,11 @@ package com.kali.scala.kafka.producer
  */
 
 import java.util.Properties
-
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.streaming.kafka.{KafkaUtils, OffsetRange}
+import org.apache.spark.{TaskContext, SparkContext, SparkConf}
+import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils, OffsetRange}
 import kafka.serializer.StringDecoder
+import sys.process._
 
 object ScalaKafkaConsumerRDD {
 
@@ -27,15 +27,29 @@ object ScalaKafkaConsumerRDD {
     val numThreads=1
     val broker="localhost:9092"
     val kafkaParams = Map[String, String]("metadata.broker.list" -> broker)
+    val kafkaProps = new Properties()
 
-    // change these values to offsets that actually exist for the topic
+    //Get max offset value from kafka logs folder using shell commands this is just for test , in rela time values should be saved in audit database and used as incremental jobs
+    val maxoffset="C:\\Users\\kalit_000\\Desktop\\kafka\\kafka_2.11-0.8.2.1\\bin\\windows\\kafka-run-class.bat kafka.tools.GetOffsetShell  --broker-list localhost:9092 --partitions 0 --topic first --time -1".!!
+    val maxoffsetValue=Seq(maxoffset).map(x =>x.split("\\:")).map(x => x(2)).toString().replace("List(","").replace(")","").replace(" ","")
+    println("max offset value:-%s".format(maxoffsetValue.trim.toInt))
+
+    //Get min offset value from kafka logs folders using shell commands this is just for test , in rela time values should be saved in audit database and used as incremental jobs
+    val minoffset="C:\\Users\\kalit_000\\Desktop\\kafka\\kafka_2.11-0.8.2.1\\bin\\windows\\kafka-run-class.bat kafka.tools.GetOffsetShell  --broker-list localhost:9092 --partitions 0 --topic first --time -2".!!
+    val minoffsetValue=Seq(minoffset).map(x =>x.split("\\:")).map(x => x(2)).toString().replace("List(","").replace(")","").replace(" ","")
+    println("mix offset value:-%s".format(minoffsetValue.trim.toInt))
+
+    // change these values to offsets that actually exist for the topic, you can extract these values from audit database if stored
     val offsetRanges = Array(
-        OffsetRange(topic.toSet.head,0,200,349)
+        OffsetRange(topic.toSet.head,0,minoffsetValue.trim.toInt,maxoffsetValue.trim.toInt)
       )
 
 
-    val rdd= KafkaUtils.createRDD[String, String, StringDecoder, StringDecoder](sc, kafkaParams, offsetRanges)
-    rdd.collect.foreach(println)
+    val lines= KafkaUtils.createRDD[String, String, StringDecoder, StringDecoder](sc, kafkaParams, offsetRanges)
+
+    lines.collect.foreach(println)
+
+    println("Sum of rows consumes today:-%d".format(lines.count()))
     sc.stop()
 
   }
